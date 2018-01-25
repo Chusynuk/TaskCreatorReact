2fd1a14262380bd7913354310a7ddb5c6b5fcc46
Jesús Eloi <jesuseloi@MacBook-Pro-de-Jesus.local>
2018-01-25 23:03:05 +0100

    changes-on-form-component

---
+++ client/src/components/Dropdown.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import DropDownMenu from 'material-ui/DropDownMenu';
+import MenuItem from 'material-ui/MenuItem';
+
+const DropDown = () => {
+
+  <DropDownMenu>
+     <MenuItem primaryText="Germany" />
+      <MenuItem  primaryText="Netherlands" />
+  </DropDownMenu>
+}
+
+
+export default DropDown;
--- client/src/components/Form.js
+++ client/src/components/Form.js
@@ -1,151 +1,116 @@
-import React from "react";
-import TextField from "material-ui/TextField"; // It's a good practice to load every component individually, to improve the perfomance
-import RaisedButton from "material-ui/RaisedButton";
+import React from 'react';
 import DatePicker from 'material-ui/DatePicker';
+import MyCustomFields from './MyCustomFields';
 
-const divStyle = {
-  paddingTop: "5vh"
+const divStyle = { // styles inline because there are just a few, and don't want to overload the application using another file for the css
+  marginTop: "5vh"
 };
 
-const buttonStyle = {
 
-  marginTop: "3vh",
-  marginBottom: "5vh"
-};
+class Form extends React.Component {
+  // state = {
+  //   DeliveryAt: "",
+  //   DeliveryAtError: "",
+  //   RecipientName: "",
+  //   RecipientNameError: "",
+  //   RecipientStreet: "",
+  //   RecipientStreetError: "",
+  //   RecipientCity: "",
+  //   RecipientCityError: "",
+  //   RecipientState: "",
+  //   RecipientStateError: "",
+  //   RecipientCountry: "",
+  //   RecipientCountryError: "",
+  //   RecipientZipcode: "",
+  //   RecipientZipcodeError: "",
+  //   RecipientPhone: "",
+  //   RecipientPhoneError: ""
+  // };
 
-export default class Form extends React.Component {
-  state = {
-    DeliveryAt: "",
-    DeliveryAtError: "",
-    RecipientName: "",
-    RecipientNameError: "",
-    RecipientStreet: "",
-    RecipientStreetError: "",
-    RecipientCity: "",
-    RecipientCityError: "",
-    RecipientState: "",
-    RecipientStateError: "",
-    RecipientCountry: "",
-    RecipientCountryError: "",
-    RecipientZipcode: "",
-    RecipientZipcodeError: "",
-    RecipientPhone: "",
-    RecipientPhoneError: ""
-  };
+  // change = e => {
+  //   this.props.onChange({
+  //     [e.target.name]: e.target.value
+  //   });
+  //   this.setState({
+  //     [e.target.name]: e.target.value
+  //   });
+  // };
 
-  change = e => {
-    // this.props.onChange({
-    //   [e.target.name]: e.target.value
-    // });
-    this.setState({
-      [e.target.name]: e.target.value
-    });
-  };
+  // validate = () => {
+  //   let isError = false;
+  //   const errors = {
+  //     DeliveryAtError: "",
+  //     RecipientNameError: "",
+  //     RecipientStreetError: "",
+  //     RecipientCityError: "",
+  //     RecipientStateError: "",
+  //     RecipientCountryError: "",
+  //     RecipientZipcodeError: "",
+  //     RecipientPhoneError: ""
+  //   };
 
-  validate = () => {
-    let isError = false;
-    const errors = [{
-      DeliveryAtError: ""},{
-      RecipientNameError: ""},{
-      RecipientStreetError: ""},{
-      RecipientCityError: ""},{
-      RecipientStateError: ""},{
-      RecipientCountryError: ""},{
-      RecipientZipcodeError: ""},{
-      RecipientPhoneError: ""
-    }];
+  //   if (this.state.DeliveryAt.length < 1) {
+  //     isError = true;
+  //     errors.DeliveryAtError = "This field is required";
+  //   }
+  //
+  //   this.setState({
+  //     ...this.state,
+  //     ...errors
+  //   });
+  //
+  //   return isError;
+  // };
 
-    if (this.state.DeliveryAt.length < 1) {
-      isError = true;
-      errors.DeliveryAtError = "This field is required";
-    }
-    if (this.state.RecipientName.length < 1) {
-      isError = true;
-      errors.RecipientNameError = "This field is required";
-    }
-    if (this.state.RecipientStreet.length < 1) {
-      isError = true;
-      errors.RecipientStreetError = "This field is required";
-    }
-    if (this.state.RecipientCity.length < 1) {
-      isError = true;
-      errors.RecipientCityError = "This field is required";
-    }
-    if (this.state.RecipientState.length < 1) {
-      isError = true;
-      errors.RecipientStateError = "This field is required";
-    }
-    if (this.state.RecipientCountry.length < 1) {
-      isError = true;
-      errors.RecipientCountryError = "This field is required";
-    }
-    if (this.state.RecipientZipcode.length < 1) {
-      isError = true;
-      errors.RecipientZipcodeError = "This field is required";
-    }
-    if (this.state.RecipientPhone.length < 1) {
-      isError = true;
-      errors.RecipientPhoneError = "This field is required";
-    }
-
-
-    this.setState({
-      ...this.state,
-      ...errors
-    });
-
-    return isError;
-  };
-
-  onSubmit = e => {
-    e.preventDefault();
-
-    const err = this.validate();
-    if (!err) {
-      // clear form
-      this.setState({
-        DeliveryAt: "",
-        DeliveryAtError: "",
-        RecipientName: "",
-        RecipientNameError: "",
-        RecipientStreet: "",
-        RecipientStreetError: "",
-        RecipientCity: "",
-        RecipientCityError: "",
-        RecipientState: "",
-        RecipientStateError: "",
-        RecipientCountry: "",
-        RecipientCountryError: "",
-        RecipientZipcode: "",
-        RecipientZipcodeError: "",
-        RecipientPhone: "",
-        RecipientPhoneError: ""
-      });
-    }
-  };
-
-
-
+  // onSubmit = e => {
+  //   e.preventDefault();
+  //
+  //   const err = this.validate();
+  //   if (!err) {
+  //     // clear form
+  //     this.setState({
+  //       DeliveryAt: "",
+  //       DeliveryAtError: "",
+  //       RecipientName: "",
+  //       RecipientNameError: "",
+  //       RecipientStreet: "",
+  //       RecipientStreetError: "",
+  //       RecipientCity: "",
+  //       RecipientCityError: "",
+  //       RecipientState: "",
+  //       RecipientStateError: "",
+  //       RecipientCountry: "",
+  //       RecipientCountryError: "",
+  //       RecipientZipcode: "",
+  //       RecipientZipcodeError: "",
+  //       RecipientPhone: "",
+  //       RecipientPhoneError: ""
+  //     });
+  //   }
+  // };
 
   render() {
-    return (<form className="Table" >
-      <DatePicker style={divStyle} hintText="Delivery at" mode="landscape"/>
+    return (<form>
+      <DatePicker style={divStyle} autoOk={true} hintText="Delivery at" mode="landscape"/>
 
-      <TextField  name="RecipientName" hintText="Recipient Name" errorText={this.state.RecipientNameError} />
+      <MyCustomFields></MyCustomFields>
+      {/* <TextField name="RecipientName" hintText="Recipient Name" value={this.state.RecipientName} errorText={this.state.RecipientNameError}/>
       <br/>
-      <TextField name="RecipientStreet" hintText="Recipient Street" value={this.state.RecipientStreet} errorText={this.state.RecipientStreetError} />
+      <TextField name="RecipientStreet" hintText="Recipient Street" value={this.state.RecipientStreet} errorText={this.state.RecipientStreetError}/>
       <br/>
-      <TextField name="RecipientCity" hintText="Recipient City" value={this.state.RecipientCity} errorText={this.state.RecipientCityError} />
+      <TextField name="RecipientCity" hintText="Recipient City" value={this.state.RecipientCity} errorText={this.state.RecipientCityError}/>
       <br/>
-      <TextField name="RecipientState" hintText="Recipient State" value={this.state.RecipientState} errorText={this.state.RecipientStateError} />
+      <TextField name="RecipientState" hintText="Recipient State" value={this.state.RecipientState} errorText={this.state.RecipientStateError}/>
       <br/>
-      <TextField name="RecipientCountry" hintText="Recipient Country" value={this.state.RecipientCountry} errorText={this.state.RecipientCountryError} />
+      <TextField name="RecipientCountry" hintText="Recipient Country" value={this.state.RecipientCountry} errorText={this.state.RecipientCountryError}/>
       <br/>
-      <TextField name="RecipientZipcode" hintText="Recipient Zipcode" value={this.state.RecipientZipcode} errorText={this.state.RecipientZipcodeError} />
+      <TextField name="RecipientZipcode" hintText="Recipient Zipcode" value={this.state.RecipientZipcode} errorText={this.state.RecipientZipcodeError}/>
       <br/>
-      <TextField name="RecipientPhone" hintText="Recipient Phone" value={this.state.RecipientPhone} errorText={this.state.RecipientPhoneError} />
-      <br/>
-      <RaisedButton style={buttonStyle} label="Submit" onClick={e => this.onSubmit(e)} primary="primary"/>
+      <TextField name="RecipientPhone" hintText="Recipient Phone" value={this.state.RecipientPhone} errorText={this.state.RecipientPhoneError}/>
+      <br/> */}
+      {/* <RaisedButton style={buttonStyle} label="Submit" onClick={e => this.onSubmit(e)} backgroundColor="pink"/> */}
     </form>);
   }
 }
+
+export default Form;
